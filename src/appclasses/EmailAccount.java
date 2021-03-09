3e46e74aa21699756d8f08202d042c7f72676106
Dillon DeGarmo <cddegarmo@gmail.com>
2021-03-09 10:08:24 -0600

    Rewrite EmailAccount for better encapsulation

--- src/appclasses/EmailAccount.java
+++ src/appclasses/EmailAccount.java
@@ -1,55 +1,99 @@
 package appclasses;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class EmailAccount {
-    private static final int MAX_USERS = 2500;
+    private static final int MAX_USERS = 100;
     private static int numOfUsers = 0;
-    private static final String COMPANY_ADDRESS = "traxus.com";
     
-    private String firstName;
-    private String lastName;
-    private String userName;
+    private final String firstName;
+    private final String lastName;
+    private final String userName;
     private String password;
-    private Company.Department department;
-    private String email;
-    private int mailboxCapacity = 500;
-    private List<String> inbox;
-    private List<String> sent;
-    private String signature;
+    private final String companyName;
+    private int department;
+    private String emailAddress;
+    private int mailboxCurrent = 0;           // in mb
+    private final int mailboxCapacity = 500;  // in mb
+    private final List<String> inbox;
+    private final List<String> sent;
 
-    private EmailAccount(String firstName, String lastName, Company.Department dep) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        department = dep;
-        userName = generateUserName();
-        password = generatePassword(8);
+    public enum Department {
+        SALES(1),
+        DEVELOPMENT(2),
+        ACCOUNTING(3),
+        NONE(0);
+
+        private int code;
+        Department(int value) {
+            code = value;
+        }
+        public int getCode() { return code; }
     }
 
-    public static EmailAccount createEmail() {
+    private EmailAccount() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter new user's first name: ");
+        firstName = s.nextLine();
+        System.out.print("Enter user's last name: ");
+        lastName = s.nextLine();
+        userName = generateUserName();
+        password = generatePassword(8);
+        System.out.print("Enter company name: ");
+        companyName = s.nextLine();
+        department = setDepartment();
+        emailAddress = generateAddress();
+        inbox = new ArrayList<>();
+        sent = new ArrayList<>();
+        numOfUsers++;
+    }
+
+    public static EmailAccount create() {
         if(numOfUsers < MAX_USERS)
             return new EmailAccount();
         else
-            throw new IllegalStateException("Organization's servers maxed out. Please contact Administration.");
+            throw new IllegalStateException("Organization's servers maxed out. " +
+                    "Please contact administration.");
     }
 
-    private String setDepartment(){
-        System.out.print("Enter department:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for None\n");
+    public static int getMaxUsers()   { return MAX_USERS;  }
+    public static int getNumOfUsers() { return numOfUsers; }
+
+    public String getFirstName() { return firstName;    }
+    public String getLastName()  { return lastName;     }
+    public String getUserName()  { return userName;     }
+    public String getPassword()  { return password;     }
+    public String getEmail()     { return emailAddress; }
+
+    public String getDepartment() {
+        for(Department d : Department.values()) {
+            if(department == d.code)
+                return d.toString();
+        }
+        return "Department code invalid. Please update value.";
+    }
+
+    public int getMailboxSpace() {
+        return mailboxCapacity - mailboxCurrent;
+    }
+
+    private int setDepartment(){
+        System.out.print("Enter department:\n1 for Sales\n2 for Development" +
+                "\n3 for Accounting\n0 for None\n");
+        System.out.println();
         Scanner in = new Scanner(System.in);
-        int departmentChoice = in.nextInt();
-        if(departmentChoice == 1)
-            return "Sales";
-        else if(departmentChoice == 2)
-            return "Development";
-        else if(departmentChoice == 3)
-            return "Accounting";
-        else
-            return "";
+        return in.nextInt();
     }
 
     private String generateUserName() {
         return this.firstName.charAt(0) + this.lastName;
+    }
+
+    private String generateAddress() {
+        return userName + "@" + companyName.toLowerCase() + ".com";
     }
 
     private String generatePassword(int length){
@@ -64,60 +108,28 @@ public class EmailAccount {
 
     public void send(EmailAccount recipient, String message) {
         recipient.receive(message);
-        sent.add(message);
+        if(getMailboxSpace() >= 2){
+            sent.add(message);
+            mailboxCurrent += 2;
+        }
+        else
+            System.out.println("Mailbox full. Please clear out space in 'sent' folder.");
     }
 
     public void receive(String message) {
-        inbox.add(message);
-    }
-
-    public void generateSignature() {
-        signature = String.format("%s %s\n%s",
-                formalize(firstName), formalize(lastName),
-                formalize(department));
-    }
-
-    private String formalize(String s) {
-        String result = s.substring(0, 1).toUpperCase() +
-                s.substring(1).toLowerCase();
-        return result;
-    }
-
-    public static int getMaxUsers()          { return MAX_USERS;       }
-    public static int getNumOfUsers()        { return numOfUsers;      }
-    public static String getCompanyAddress() { return COMPANY_ADDRESS; }
-
-    public String getFirstName()             { return firstName;       }
-    public String getLastName()              { return lastName;        }
-    public String getUserName()              { return userName;        }
-    public String getPassword()              { return password;        }
-    public String getDepartment()            { return department;      }
-    public String getEmail()                 { return email;           }
-    public int getMailboxCapacity()          { return mailboxCapacity; }
-
-    @Override
-    public boolean equals(Object o) {
-        if(o == this)
-            return true;
-        if(!(o instanceof EmailAccount))
-            return false;
-        EmailAccount em = (EmailAccount) o;
-        return em.firstName.equals(firstName) && em.lastName.equals(lastName) &&
-                em.password.equals(password);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = firstName.hashCode();
-        result = 31 * result + lastName.hashCode();
-        result = 31 * result + password.hashCode();
-        return result;
+        if(getMailboxSpace() >= 2) {
+            inbox.add(message);
+            mailboxCurrent += 2;
+        }
+        else
+            System.out.println("Mailbox full. Recipient needs to clear space in inbox.");
     }
 
     @Override
     public String toString(){
-        return "EMPLOYEE: " + formalize(firstName) + " " + formalize(lastName) +
-                "\nEMAIL: " + email +
-                "\nMAILBOX CAPACITY: " + mailboxCapacity + "mb";
+        return "EMPLOYEE: " + firstName.toUpperCase() + " " + lastName.toUpperCase() +
+                "\nEMAIL: " + emailAddress.toUpperCase() +
+                "\nMAILBOX CAPACITY: " + mailboxCapacity + "mb" +
+                "\nMAILBOX SPACE: " + (mailboxCapacity - mailboxCurrent) + "mb";
     }
 }
