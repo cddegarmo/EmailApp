ddb6486eab6e0b939bc49b8e8872a1ef73908313
Dillon DeGarmo <cddegarmo@gmail.com>
2021-05-06 12:00:04 -0500

    Rearrange files, eliminate unnecessary package

---
+++ src/main/java/Register.java
@@ -0,0 +1,18 @@
+package user;
+import main.java.*;
+import main.java.Employee.*;
+
+import java.nio.file.Path;
+
+import static main.java.Employee.Sex.*;
+
+public class Register {
+    public static void main(String[] args) {
+        Company apache = Company.found();
+        apache.loadEmployees();
+        apache.printEmployees();
+        System.out.println(apache.employeesByDepartment().toString());
+        System.out.println(apache.salaryByDepartment().toString());
+        System.out.println(apache.countByDepartment().toString());
+    }
+}
